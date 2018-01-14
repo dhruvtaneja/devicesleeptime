8c189648628e861aa66db0cc3b85bc3c66bd5adc
Dhruv Taneja <dhruvtaneja1209@gmail.com>
2018-01-14 17:22:06 +0530

    Add get system settings long extension to Context

---
+++ app/src/main/java/taneja/dhruv/devicesleeptime/ContextExtensions.kt
@@ -0,0 +1,12 @@
+package taneja.dhruv.devicesleeptime
+
+import android.content.Context
+import android.provider.Settings
+
+/**
+ * Created by dhruvtaneja on 14/01/18.
+ */
+
+
+fun Context.getSystemSettingsLong(setting : String, default : Long) =
+        Settings.System.getLong(contentResolver, setting, default)
